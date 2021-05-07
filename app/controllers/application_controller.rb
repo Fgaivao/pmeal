f7191c6385f25d295a121bba65205d5e09a8fec3
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2021-05-07 12:02:38 +0100

    commit messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,18 +31,8 @@ end
 end
 
   def authenticate_user!(options = {})
-    if request.headers['Authorization'].present?
-      authenticate_or_request_with_http_token do |token|
-        begin
-          jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
-
-          @current_user_id = jwt_payload['id']
-        rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
-          head :unauthorized
-    end
+    head :unauthorized unless signed_in?
   end
-end
-end
 
   def current_user
     @current_user ||= super || User.find(@current_user_id)
