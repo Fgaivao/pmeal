7db8d9f67612d808d15ded242c8bf4ad8653cad4
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2021-05-07 11:37:54 +0100

    auth to secauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,8 +31,18 @@ end
 end
 
   def authenticate_user!(options = {})
-    head :unauthorized unless signed_in?
+    if request.headers['Authorization'].present?
+      authenticate_or_request_with_http_token do |token|
+        begin
+          jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+
+          @current_user_id = jwt_payload['id']
+        rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+          head :unauthorized
+    end
   end
+end
+end
 
   def current_user
     @current_user ||= super || User.find(@current_user_id)
