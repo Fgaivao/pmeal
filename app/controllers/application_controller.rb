94437e74f54d1c21c79dab6bab53f9655de1b204
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2021-05-10 12:48:49 +0100

    secrets to credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ private
     if request.headers['Authorization'].present?
       authenticate_or_request_with_http_token do |token|
         begin
-          jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+          jwt_payload = JWT.decode(token, Rails.application.credentials.secret_key_base).first
 
           @current_user_id = jwt_payload['id']
         rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
@@ -39,6 +39,6 @@ end
   end
 
   def signed_in?
-    !!current_user
+    @current_user_id.present?
   end
 end
