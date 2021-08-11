fbc052625a32614170c674d80f431bcd888cf6eb
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2021-08-11 12:38:18 +0100

    once a day in model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,12 @@
 class Meal < ApplicationRecord
   belongs_to :user
+  validate :date_scope
+
+  private
+
+   def date_scope
+    if Meal.where("user_id = ? AND DATE(created_at) = DATE(?)", self.user_id, Time.now).all.any?
+      errors.add(:user_id, "Só pode criar uma refeição por dia")
+    end
+  end
 end
