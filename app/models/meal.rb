48846e814e2b45b049678812299ddb23a01eb8be
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2021-08-12 10:34:13 +0100

    remove user id from error

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -6,7 +6,7 @@ class Meal < ApplicationRecord
 
    def date_scope
     if Meal.where("user_id = ? AND DATE(created_at) = DATE(?)", self.user_id, Time.now).all.any?
-      errors.add(:user_id, "Só pode criar uma refeição por dia")
+      errors.add("Só pode criar uma refeição por dia")
     end
   end
 end
