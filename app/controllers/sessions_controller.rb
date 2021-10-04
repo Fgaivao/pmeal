4443b3857f7694b6229b83edf81625d3582ec44e
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2021-10-04 16:13:11 +0100

    find by name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < Devise::SessionsController
   def create
-    user = User.find_by_email(sign_in_params[:name])
+    user = User.find_by_name(sign_in_params[:name])
 
     if user && user.valid_password?(sign_in_params[:password])
       @current_user = user
