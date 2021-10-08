229040fa0452e93f153564356575f52e94ad81d3
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2021-10-08 15:13:23 +0100

    rm index users db

---
+++ db/migrate/20211008141134_add_change_remove_index.rb
@@ -0,0 +1,9 @@
+class AddChangeRemoveIndex < ActiveRecord::Migration[6.0]
+def up
+    remove_index :users, :email
+  end
+
+  def down
+    add_index :users, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_08_113321) do
+ActiveRecord::Schema.define(version: 2021_10_08_141134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2021_10_08_113321) do
     t.string "diet"
     t.string "goal"
     t.string "username"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
