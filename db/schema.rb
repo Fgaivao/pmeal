af7d1d53fb6663cfed68e28973288437ba7e9cf6
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2021-10-08 12:20:40 +0100

    rm email add username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_105856) do
+ActiveRecord::Schema.define(version: 2021_10_08_111808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,8 +50,10 @@ ActiveRecord::Schema.define(version: 2021_09_23_105856) do
     t.integer "height"
     t.string "diet"
     t.string "goal"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "meals", "users"
