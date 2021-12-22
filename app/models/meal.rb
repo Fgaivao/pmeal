a106f322c9c1c51adc0848e1901c6f89c2fbf0c6
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2021-12-22 19:16:11 +0000

    meal a day

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,13 +1,13 @@
 class Meal < ApplicationRecord
   belongs_to :user
   validates :name, :proteins, :hydrates, :vegetables, :protconfection, :hydconfection, :hortconfection, :hydperc, :hortperc, presence: true
- # validate :date_scope
+  validate :date_scope
 
- # private
+  private
 
-  # def date_scope
-   # if Meal.where("user_id = ? AND DATE(created_at) = DATE(?)", self.user_id, Time.now).all.any?
-    #  errors.add("Só pode criar uma refeição por dia")
-    #end
-  #end
+   def date_scope
+    if Meal.where("user_id = ? AND DATE(created_at) = DATE(?)", self.user_id, Time.now).all.any?
+      errors.add("Só pode criar uma refeição por dia")
+    end
+  end
 end
